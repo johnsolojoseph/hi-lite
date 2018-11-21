622ad16a5b65b030e173f808ab9425ab5e4f892b
John Solo <johnsolojoseph@gmail.com>
2018-11-20 17:28:31 -0800

    Removing database credentials for production

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'YOUR-CREDENTIALS-HERE'}
+  module.exports = {mongoURI: 'YOUR-CREDENTIALS-HERE-'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/hilite-dev'}
 }
